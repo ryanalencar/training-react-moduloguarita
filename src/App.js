932e878299b1e8591a97c554f4ad8511e12a040c
leguass7 <leandro.sbrissa@hotmail.com>
2020-04-21 20:36:45 -0300

    feat: add PageHome

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './assets/logo.svg';
+import PageHome from './pages/Home';
+
 import './assets/styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>&quot;./src/pages/Home.js&quot;</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/avatarsolucoes/training-react-moduloguarita.git"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
-      </header>
-    </div>
+    <>
+      <PageHome />
+    </>
   );
 }
 

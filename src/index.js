a577a10973555e6e2452096b6e1e54c3569fcf52
leguass7 <leandro.sbrissa@hotmail.com>
2020-04-22 16:35:02 -0300

    feat: removi funcoes nao usadas

--- src/index.js
+++ src/index.js
@@ -3,17 +3,9 @@ import ReactDOM from 'react-dom';
 import './assets/styles/index.css';
 import App from './App';
 
-import { something2 } from './helper/utils';
-
-something2(1, 2);
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-export function Teste() {
-  return <h1>teste</h1>;
-}
